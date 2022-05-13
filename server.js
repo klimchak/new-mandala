e55ce5da9e83eee9ddd575a541eb20f186b48a1e
Ilya Klimchyk <iklimchyk@elinext.com>
2022-05-13 14:25:17 +0400

    removed express  from master

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/new-mandala'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/new-mandala/index.html'));
-});
-app.listen(process.env.PORT || 8080);
